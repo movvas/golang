d4e0ae9b07fdd90d43e65f2111dae9decb64346a
Sasidhar Movva <smovva@gmail.com>
2015-02-03 15:35:05 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Excercies for Learning Go Programming Language (golang)
+
+These excercise are based on
+http://www.codingbat.com and
+https://developers.google.com/edu/python/
+
+## Boolean Operators
+This directory contains excercises that help you understand boolean operators
+
+## How to use
+Clone the project and do "go test" in the subfolder of your choice.
+
+
+
+## Contributors
+If you would like to add addtional excercises, please drop me a line @msasi
+
+## License
---
+++ exercises/boolean_operators/sleepin_test.go
@@ -0,0 +1,54 @@
+// The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+//
+// SleepIn(False, False) → True
+// SleepIn(True, False) → False
+// SleepIn(False, True) → True
+
+package sleepin
+
+import "testing"
+
+var sleepInTests = []struct {
+	weekday  bool
+	vacation bool
+	out      bool
+}{
+	{
+		weekday:  true,
+		vacation: true,
+		out:      true,
+	},
+	{
+		weekday:  false,
+		vacation: true,
+		out:      true,
+	},
+	{
+		weekday:  true,
+		vacation: false,
+		out:      false,
+	},
+	{
+		weekday:  false,
+		vacation: false,
+		out:      true,
+	},
+}
+
+func TestSleepIn(t *testing.T) {
+  var rcvd bool
+	for  test := range sleepInTests {
+    rcvd = SleepIn(sleepInTests[test].weekday, sleepInTests[test].vacation)
+		if rcvd != sleepInTests[test].out {
+			t.Error("Test Failed", sleepInTests[test].weekday,sleepInTests[test].vacation, sleepInTests[test].out,rcvd)
+     }
+	}
+}
+
+func SleepIn(weekday, vacation bool) bool {
+
+  if (! weekday) || vacation {
+    return true;
+  }
+  return false;
+}
